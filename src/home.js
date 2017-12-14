370a2e8c8cea8d85af2c32dd59b6a1e0ad2f68c6
mistermappy123 <mikhal.gelo@gmail.com>
2017-12-13 21:11:13 -0500

    nav menu

---
+++ src/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+  render(){
+    return(
+      <div
+       class='main-body'>
+      <h1>Filler Content</h1>
+      </div>
+    )
+  }
+}
+
+export default Home;
---
+++ src/myStore.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class MyStore extends Component {
+
+  render(){
+    return(
+      <div
+        class='store-page'>
+        <h1>Store Page</h1>
+      </div>
+    )
+  }
+}
+
+export default MyStore;
