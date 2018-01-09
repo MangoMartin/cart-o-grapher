c86a068c9001391b7f1050e55927605c8cb44ce3
Sean McNeeley <sean21mcn@gmail.com>
2018-01-09 15:34:28 -0500

    merging master branch

--- src/Nav/index.jsx
+++ src/Nav/index.jsx
@@ -1,79 +1,70 @@
 import React, { Component } from 'react';
 import {Link, Route, Switch} from 'react-router-dom';
-import MyStore from '../StoreProfile';
-import Settings from '../ProfileDesign';
+import CreateShop from '../ProfileDesign';
 import Home from '../Cartograph';
-import Maps from '../Cartograph/Map.jsx';
+import Maps from '../Cartograph/Map.js';
+import './index.css';
 
 class Container extends Component {
 
-  constructor(){
-    super();
+    constructor(){
+        super();
+        this.removeMap = this.removeMap.bind(this);
+    }
 
-    this.removeMap = this.removeMap.bind(this);
+    render() {
+        return(
+            <div className='header'>
+                <Link to='/' onClick={this.removeMap}>
+                    <div className='header-logo'>
+                        <img className='logo' src={require('./logo.jpg')} alt='' />
+                        <h1>Cart-o-Grapher</h1>
+                    </div>
+                </Link>
+                <div className='header-nav'>
+                    <ul>
+                    {this.props.children}
+                        <Link to='/create' onClick={this.removeMap}>
+                            <li>Create Shop</li>
+                        </Link>
+                        <Link to='/logout' onClick={this.removeMap}>
+                            <li>Log Out</li>
+                        </Link>
+                    </ul>
 
-  }
+                    <Switch>
+                        <Route
+                            exact
+                            path="/"
+                            component={Home}
+                        />
+                        <Route
+                            path="/create"
+                            component={CreateShop}
+                        />
+                        <Route
+                            path="/logout"
+                            component={Logout}
+                        />
+                    </Switch>
+                </div>
+            </div>
+        )
+    }
 
-  render() {
-
-    return(
-      <div
-        className='header'>
-        <div
-          className='header-logo'>
-          <h1>Cart-o-Grapher</h1>
-        </div>
-        <div
-          className='header-nav'>
-          <ul>
-          {this.props.children}
-            <Link to='/'><li>Home</li></Link>
-            <Link to='/mystore' onClick={this.removeMap}><li>My Store</li></Link>
-            <Link to='/settings'><li>Settings</li></Link>
-            <Link to='/logout'><li>Log Out</li></Link>
-          </ul>
-
-          <Switch>
-          <Route
-              exact
-              path="/"
-              component={Home}
-            />
-            <Route
-              path="/mystore"
-              component={MyStore}
-            />
-            <Route
-              path="/settings"
-              component={Settings}
-            />
-            <Route
-              path="/logout"
-              component={Logout}
-            />
-          </Switch>
-        </div>
-      </div>
-    )
-  }
-
-  removeMap() {
-    this.map.remove();
-  }
+    removeMap() {
+        this.map.remove();
+    }
 }
 
-
-
-
 class Logout extends Component {
+    render(){
+        return(
+            <div>
 
-  render(){
-    return(
-      <div>
-
-      </div>
-    )
-  }
+            </div>
+        )
+    }
 }
 
 export default Container;
