5cf60bf8e62c0ff9bc216900dd4ce6a0df1154ca
Raymond Ajax Metrulis <raymondajaxmetrulis@gmail.com>
2018-01-11 11:57:23 -0700

    Other map removed succesfully by putting Map.js code into index.jsx. Uncaught TypeError does not crash the app, but could in the future. Has a lot to do with setting the keys.

--- client/src/Cartograph/index.jsx
+++ client/src/Cartograph/index.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import MappedShops from './LocalShops';
-import MapContainer from './Map.jsx';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
+import Markers from './marker.js';
 import './index.css';
 
 export default class Home extends Component {
@@ -9,10 +12,38 @@ export default class Home extends Component {
     super();
     this.state = {
       tests: [],
+      addresses: []
     };
   }
 
   render(){
+    const provider = new OpenStreetMapProvider();
+        provider.search({query: '401 East 60th Street'})
+                .then(function(result){
+                  console.log(result);
+                })
+        const searchControl = new GeoSearchControl({
+          provider : provider,
+          style: 'button',
+          autoClose: true,
+          keepResult: true,
+          maxMarker: 3
+        });
+
+         const map = L.map('map', { zoom: 5, minZoom: 2})
+                     .addControl(searchControl)
+                     .setView([51.505, -0.09], 13);
+
+          L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            noWrap: true,
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          }).addTo(map);
+
+          for (var i = 0; i < Markers.length; i++){
+          L.marker([Markers[i].lat, Markers[i].lng]).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
+          }
     const { tests } = this.state;
     return(
       <div className='main-body'>
@@ -24,14 +55,14 @@ export default class Home extends Component {
             view nearby stores located on the map and their respective store profiles
             below.
             </p>
-            {tests.map((test, index) =>
-              <div key={index}>
+            {tests.map((test) =>
+              <div key={test.username}>
                 {test.username}
               </div>
             )}
       		</div>
-      		<MapContainer 
-      		/>
+      		<div id='map'>
+      		</div>
       	</div>
       	<MappedShops
       	/>
