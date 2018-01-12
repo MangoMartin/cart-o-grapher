824beaa97eeb9ec45aa8cfe728edc184b5b54be6
Raymond Ajax Metrulis <raymondajaxmetrulis@gmail.com>
2018-01-12 06:02:06 -0700

    Set up routes for login and signup.

---
+++ routes/loginSignup.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+const models  = require('../db');
+
+module.exports = function(passport) {
+	router.post('/', function(req, res, next) {
+		passport.authenticate('local-signup', function(err, user, info) {
+			if (err) {
+			  	return next(err); 
+			}
+			if (!user) {
+			   	return next({ error : true, message : info });
+			}
+			req.login(user, function(loginErr) {
+			   	if (loginErr) {
+			     	return next(loginErr);
+			   	}
+			 	return res.redirect('/api/');
+			}); 
+		})(req, res, next);
+		passport.authenticate('local-login', function(err, user, info) {
+			if (err) {
+			  	return next(err); 
+			}
+			if (! user) {
+			   	return next({ error : true, message : info });
+			}
+			req.login(user, function(loginErr) {
+			    if (loginErr) {
+					return next(loginErr);
+			    } 
+
+				res.cookie('jwt', user.token);
+				res.cookie('username', user.username);
+				res.cookie('userid', user.id);
+ 
+				return res.redirect('/api/owner/');
+			}); 
+		})(req, res, next);
+	});
+
+  	return router;
+};
+
