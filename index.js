cb45462d126662e3d37aea4afbacce3e61a201dd
mistermappy123 <mikhal.gelo@gmail.com>
2018-01-13 01:27:38 -0500

    renders Creat-react-app now

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', (req,res)=>{
+app.get('/users', (req,res)=>{
 	res.json([
 			{id:1, name:"green"},
 			{id:2, name:"blue"}
