47ca6ffc9a6d906e795ab0451063ce46b005d885
Raymond Ajax Metrulis <raymondajaxmetrulis@gmail.com>
2018-01-15 16:48:55 -0700

    commit to deploy

--- client/src/ProfileDesign/LoginSignup.jsx
+++ client/src/ProfileDesign/LoginSignup.jsx
@@ -5,17 +5,14 @@ class LoginSignup extends Component {
  constructor() {
     super();
   }
-componentDidMount() {
-    this.handleSubmit();
-  }
+  
 handleSubmit = (event) => {
     event.preventDefault();
-    fetch('/api/singup');
   }
 
   render() {
     return (
-      <form method='POST' encType='application/x-www-form-urlencoded' onSubmit={this.handleSubmit}>
+      <form method='POST' action='/api/signup' encType='application/x-www-form-urlencoded' onSubmit={this.handleSubmit}>
         <input type="text" placeholder="Username" ref="username" name="username"/>
         <input type="text" placeholder="Password" ref="password" name="password"/>
         <input type="submit" />
