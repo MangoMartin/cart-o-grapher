1c84a017de0c4fa2bd1b806f17a91906d271e8e6
Raymond Ajax Metrulis <raymondajaxmetrulis@gmail.com>
2018-01-15 16:05:23 -0700

    small syntax error

--- config/config.js
+++ config/config.js
@@ -17,7 +17,7 @@ const CONFIGURATION = {
     "password": "4fc7a244e823a255662fc98b21aaeba16d1b07f487976254ff437a301bf250e6",
     "host": "ec2-54-235-252-137.compute-1.amazonaws.com",
     "port": 5432,
-    "database": "ddnaoa3vvhtr63"
+    "database": "ddnaoa3vvhtr63",
     "jwtSecret": "MySuperDuperSecret",
     "jwtExpiration": 60*5, // 5 Minutes
     "saltRounds": 10
