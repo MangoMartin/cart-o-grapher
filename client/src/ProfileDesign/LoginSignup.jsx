605a79ca2202f5bf0fb7df3753fe1482e70ef0cf
Raymond Ajax Metrulis <raymondajaxmetrulis@gmail.com>
2018-01-15 16:13:43 -0700

    small syntax error

--- client/src/ProfileDesign/LoginSignup.jsx
+++ client/src/ProfileDesign/LoginSignup.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+const port = process.env.PORT;
 import './index.css';
 
 class LoginSignup extends Component {
@@ -7,7 +8,7 @@ class LoginSignup extends Component {
   }
   render() {
     return (
-      <form method='POST' action='http://localhost:3232/api/signup' encType='application/x-www-form-urlencoded'>
+      <form method='POST' action='/api/signup' encType='application/x-www-form-urlencoded'>
         <input type="text" placeholder="Username" ref="username" name="username"/>
         <input type="text" placeholder="Password" ref="password" name="password"/>
         <input type="submit" />
