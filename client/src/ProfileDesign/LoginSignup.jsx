c455f0bcaa11e19cfa5f832d58644885c2d0737a
Raymond Ajax Metrulis <raymondajaxmetrulis@gmail.com>
2018-01-15 17:36:01 -0700

    Added log in form

--- client/src/ProfileDesign/LoginSignup.jsx
+++ client/src/ProfileDesign/LoginSignup.jsx
@@ -7,11 +7,22 @@ class LoginSignup extends Component {
   }
   render() {
     return (
+      <div className='login-signup'>
+        <div className='login'>
       <form method='POST' action='/api/signup' encType='application/x-www-form-urlencoded'>
         <input type="text" placeholder="Username" ref="username" name="username"/>
         <input type="text" placeholder="Password" ref="password" name="password"/>
-        <input type="submit" />
+        <input type="submit" value="Sign Up" />
       </form>
+      </div>
+      <div className='signup'>
+      <form method='POST' action='/api' encType='application/x-www-form-urlencoded'>
+        <input type="text" placeholder="Username" ref="username" name="username"/>
+        <input type="text" placeholder="Password" ref="password" name="password"/>
+        <input type="submit" value="Log In" />
+      </form>
+      </div>
+      </div>
     );
   }
 }
