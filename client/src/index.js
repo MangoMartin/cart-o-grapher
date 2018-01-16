2044de98dd5075b6d1d0b5b7cd08568c0b66e3c8
Raymond Ajax Metrulis <raymondajaxmetrulis@gmail.com>
2018-01-16 01:21:12 -0700

    Successful jwt auth with local db login, now routes need to be protected. Pushing to production

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import Container from './Nav';
 import { BrowserRouter } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
 
 ReactDOM.render((
+	<CookiesProvider>
     <BrowserRouter>
       <Container />
     </BrowserRouter>
+     </CookiesProvider>
 ),document.getElementById('root'));
 registerServiceWorker();
