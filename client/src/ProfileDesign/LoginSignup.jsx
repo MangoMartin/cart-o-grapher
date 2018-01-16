268243285fba2b75dd0a80afe12779d1651e23dc
Raymond Ajax Metrulis <raymondajaxmetrulis@gmail.com>
2018-01-15 17:15:07 -0700

    commit to deploy

--- client/src/ProfileDesign/LoginSignup.jsx
+++ client/src/ProfileDesign/LoginSignup.jsx
@@ -7,7 +7,7 @@ class LoginSignup extends Component {
   }
   render() {
     return (
-      <form method='POST' action='https://cart-o-grapher.herokuapp.com/api/signup' encType='application/x-www-form-urlencoded'>
+      <form method='POST' action='/api/signup' encType='application/x-www-form-urlencoded'>
         <input type="text" placeholder="Username" ref="username" name="username"/>
         <input type="text" placeholder="Password" ref="password" name="password"/>
         <input type="submit" />
